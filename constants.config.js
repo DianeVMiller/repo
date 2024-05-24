04f607fdead02ec98c73b15e0dccefcf67444d5e
DianeVMiller <dianevmiller@gmail.com>
2024-05-24 13:30:39 -0700

    Add files via upload

---
+++ constants.config.js
@@ -0,0 +1,6 @@
+const CONSTANTS = {
+    chatbotTitle: "Nation Assistant",
+    chatbotName: "Nation Assistant",
+};
+
+module.exports = CONSTANTS;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'sidebar': '#003399',
+        'conversation': '#C08E6F',
+        'searchbar': '#FFFFFF',
+      },
+      textColor: {
+        'primary': '#182852',
+      }
+    },
+  },
+  variants: {
+    extend: {
+      visibility: ['group-hover'],
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
